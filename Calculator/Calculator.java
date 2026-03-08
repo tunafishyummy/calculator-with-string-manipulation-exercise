05a4d6382fac454e0ae6ba6729888f3533f72228
tunafishyum <adinoellamio@gmail.com>
2026-03-08 20:09:50 +0800

    initial commit

---
+++ Calculator/Calculator.java
@@ -0,0 +1,18 @@
+package Calculator;
+
+public class Calculator {
+    public static void main(String[] args) {
+        int num1 = 0;
+        int num2 = 0;
+        int num3 = 0;
+        int num4 = 0;
+        String opening = "Welcome to the Calculator! The Sinister Potion!";
+        String stringy1 = "";
+        String stringy2 = "";
+        String stringyyes = "yes";
+        while {
+            stringyyes = "yes";
+            System.out.println(opening);
+        }
+    }
+}
